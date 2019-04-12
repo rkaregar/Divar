c77783977a6566380502cb5143d4bfb31e8327b1
Amirreza Mozayani <amirrezamozayani@gmail.com>
2019-04-12 16:58:26 +0430

    add base.html

--- divar-site/divar/settings.py
+++ divar-site/divar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'divar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 LOGIN_URL = '/users/login/'
