0944bb5afd600fdc0c8daa7edb66a42590e2dec1
shayan <shekshaa.75@gmail.com>
2019-04-21 21:00:26 +0430

    add functionality of viewing ads

--- divar-site/ads/urls.py
+++ divar-site/ads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from ads.views import HomeView, AdvertisementCreationView
+from ads.views import HomeView, AdvertisementCreationView, AdvertisementViewView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('creation/', AdvertisementCreationView.as_view(), name='create_advertisement')
+    path('creation/', AdvertisementCreationView.as_view(), name='create_advertisement'),
+    path('view/<int:id>/', AdvertisementViewView.as_view(), name='view_advertisement')
 ]
